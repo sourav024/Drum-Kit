ae3390ee768698deefd8327fab1a33a125251950
sourav024 <59652600+sourav024@users.noreply.github.com>
2021-06-03 14:11:55 +0530

    Add initial Drum Kit website files

---
+++ Index.js
@@ -0,0 +1,67 @@
+var numberofButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberofButtons; i++) {
+
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var Clickedbtn = this.innerHTML;
+    makesound(Clickedbtn);
+
+    buttonAnimation(Clickedbtn);
+
+  });
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makesound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+function makesound(key) {
+
+  switch (key) {
+    case "w":
+      var audio = new Audio("tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+
+  }
+
+}
+
+function buttonAnimation(currentkey) {
+  var activateButton = document.querySelector("." + currentkey);
+
+  activateButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activateButton.classList.remove("pressed");
+  }, 100);
+}
